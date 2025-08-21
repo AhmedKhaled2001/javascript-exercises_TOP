45ee0d43eae908f2ea450575c3e3ae182ef9b303
AhmedKhaled2001 <dido17777@gmail.com>
2025-08-21 03:03:07 +0300

    JS Excercises 1 - 11 Complete

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) 
+{
+    
+    console.log(items);
+    return arr.filter((item) => !items.includes(item));
 };
 
 // Do not edit below this line
